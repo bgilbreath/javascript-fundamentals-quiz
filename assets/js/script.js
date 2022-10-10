fddcd43230965b81a255879d165b7ded453ac205
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2022-10-10 12:46:19 -0400

    Reformatted framework for page, began work on basic functionality for starting quiz.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,39 @@
 var startQuizBtn = document.querySelector("#start-quiz");
+var possibleAnswersEl = document.querySelector("#choices");
+var quizQuestionEl = document.querySelector("#question");
 
-startQuizBtn.addEventListener("click", function(){
-  console.log("Quiz has begun...")
-});
+var questions = [
+  {
+      question: "What does DOM stand for?",
+      possibleAnswers: [
+          "Document Object Model",
+          "Domain Object Mountain",
+          "Doolittle Orange Man",
+          "Duck Overview Management"
+      ],
+      correctAnswer: "Document Object Model"
+  }
+];
 
-  
+
+
+
+
+
+
+
+startQuizBtn.addEventListener("click", startQuiz);
+
+
+function startQuiz(){
+    var currentQuestion = questions[0];
+    quizQuestionEl.textContent = currentQuestion.question;
+
+    for (var i = 0; i <currentQuestion.possibleAnswers.length; i++){
+
+      var li = document.createElement("li");
+      li.textContent = currentQuestion.possibleAnswers[i];
+      possibleAnswersEl.appendChild(li);
+    }
+    startQuizBtn.setAttribute("style", "display:none;")
+}
